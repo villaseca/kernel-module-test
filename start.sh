38216feac0d63ca61ba13c2dfdfc068499b6292a
Tomás Villaseca <tvvillas@gmail.com>
2016-08-03 13:22:40 -0400

    Check info on loaded module

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 echo "Loading a non-existent kernel module..."
 modprobe not-a-module
 echo "Loading the i2c-mux kernel module..."
+modinfo i2c-mux
 modprobe i2c-mux
+modinfo i2c-mux
 lsmod
 
 while true
