5dd84c49958a301021e06f848e66a3a550d3ff73
Tomás Villaseca <tvvillas@gmail.com>
2016-08-03 13:15:27 -0400

    Test modprobe output on error

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+echo "Loading a non-existent kernel module..."
+modprobe not-a-module
+echo "Loading the i2c-mux kernel module..."
+modprobe i2c-mux
+lsmod
+
 while true
 do
-    echo "Loading a kernel module..."
-    modprobe i2c-mux
-    lsmod
     sleep 60s
     echo "Hello world"
 done
